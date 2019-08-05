3eac01d484d4d8ff4acd656d41222ece089efbc9
Finn Thye <piqueen@gmail.com>
2019-08-05 02:10:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(name_array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map()
+  planeteer_calls.map { |call|   }
 end
 
 def long_planeteer_calls# code an argument here
